2b53fbf8f540e590818f79e75f04b9a5f6588b15
Susana Pham <s.susana.pham@gmail.com>
2020-10-21 21:48:04 +0200

    Add fonts

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,9 +1,41 @@
+function renderCountries(data) {
+  var countries = document.querySelector('#countries')
+  if (!data) {
+    return countries.innerHTML = `<div class="error">Data not available.</div>`
+  }
+
+  console.log(data)
+  // {name: "Afghanistan", topLevelDomain: Array(1), alpha2Code: "AF", alpha3Code: "AFG", callingCodes: Array(1), …}
+
+  countries.innerHTML = data.slice(0, 10).map(country => `<div>${country.name}</div>`).join('\n')
+
+}
+
+function loadApi(fn) {
+  fetch("https://rapidapi.p.rapidapi.com/all", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "restcountries-v1.p.rapidapi.com",
+      "x-rapidapi-key": "88a847281amsh7cc2a9dc52cf77cp16561ejsnc253d8fee2cd"
+    }
+  })
+  .then(response => response.json())
+  .then(data => {
+    renderCountries(data)
+  })
+  .catch(err => {
+    console.error(err)
+    renderCountries()
+  })
+}
+
 function loadSlider() {
   $('.slider').slick({
     centerMode: true,
     centerPadding: '60px',
     slidesToShow: 3,
     dots: true,
+    variableWidth: true,
     responsive: [
       {
         breakpoint: 768,
@@ -24,5 +56,5 @@ function loadSlider() {
         }
       }
     ]
-  });
+  })
 }
